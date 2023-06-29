a3ad317e207f1778e507db1d1d0c9ae6892ac800
Emanuelngola2019 <emanuelngola33@gmail.com>
2023-06-29 09:44:48 +0100

    api create user

--- mysql.js
+++ mysql.js
@@ -4,7 +4,7 @@ var pool= mysql.createPool({
     "password":"", //process.env.MYSQL_PASSWORD,
     "host": "localhost",//process.env.MYSQL_HOST,
     "port":"3306", //process.env.MYSQL_PORT,
-    "database": "teste",//process.env.MYSQL_DATABASE,
+    "database": "helptask",//process.env.MYSQL_DATABASE,
 }
 );
  exports.pool= pool;
